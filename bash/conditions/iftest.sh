98716116f71604027b919fd90873a6e2c1953f0f
JamesKnott <jknott@boojookoo.com>
2012-07-26 21:20:03 -0500

    new bash file commit

---
+++ bash/conditions/iftest.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ 1 -eq 1 ]
+then
+    echo "Both numeric values are equal"
+
+else
+    echo "Both numeric values are not equal"
+fi
+
+if [ cat = cat ]
+then
+    echo "Both string values are equal"
+else
+    echo "Both string values are not equal"
+fi
+
