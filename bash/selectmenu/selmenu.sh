8a769d0a024354f93754d106d5fec4a31feee51a
JamesKnott <jknott@boojookoo.com>
2012-08-04 12:21:24 -0500

    Updated bash files

--- bash/selectmenu/selmenu.sh
+++ bash/selectmenu/selmenu.sh
@@ -7,16 +7,16 @@ select i in $LIST
     do
         if [ $i = "For_Loop" ]
         then
-            cat /home/joeblow/Scripts/bash/forloops/basicloop.sh
+            cat ~/Scripts/bash/forloops/basicloop.sh
         elif [ $i = "If_then_else" ]
         then
-            cat /home/joeblow/Scripts/bash/conditions/iftest.sh
+            cat ~/Scripts/bash/conditions/iftest.sh
         elif [ $i = "Select_Case" ]
         then
-            cat /home/joeblow/Scripts/bash/casestmnt/selcase1.sh
+            cat ~/Scripts/bash/casestmnt/selcase1.sh
         elif [ $i = "Echo" ]
         then
-            cat /home/joeblow/Scripts/bash/how_to_echo/helloworld.sh
+            cat ~/Scripts/bash/how_to_echo/helloworld.sh
         elif [ $i = "Quit" ]
         then
             exit
