fc33df19167b122ef8f89f94ec5693e8750bdfc9
JamesKnott <jknott@boojookoo.com>
2012-08-13 23:33:19 -0500

    Fun with python

---
+++ python/echo/helloworld.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+#message = "Hello World!"
+#print "\n\n", message, "\n\n"
+
+
+
+
---
+++ python/input/cmdline_input.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+
+print "------------------------- Life Expectancy v0.99 ---------------------------"
+name = raw_input ("What is your name ? : ")
+print name,
+age = input ("What is your age? : ")
+print name,
+expect = input("What age do you expect to live to? : ")
+print name,
+print "Based on our calculations, you will live for", expect - age, " more years"
+print "Thank you for using Life Expectancy v0.99"
+
+
+
