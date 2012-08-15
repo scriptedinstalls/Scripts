46499439ef8ed8b4aa279dd153f6efa639b881e6
JamesKnott <jknott@boojookoo.com>
2012-08-15 15:52:25 -0500

    Adding logic to lamp.sh

--- bash/conditions/check_distro.sh
+++ bash/conditions/check_distro.sh
@@ -1,14 +1,58 @@
 #!/bin/bash
 
+GetOSVersion() {
+   if [[ -n "`which sw_vers 2>/dev/null`" ]]; then
+         os_VENDOR=`sw_vers -productName`
+         os_RELEASE=`sw_vers -productVersion`
+         os_UPDATE=${os_RELEASE##*.}
+         os_RELEASE=${os_RELEASE%.*}
+         os_PACKAGE=""
+         if [[ "$os_RELEASE" =~ "10.7" ]]; then
+         os_CODENAME="lion"
+         elif [[ "$os_RELEASE" =~ "10.6" ]]; then
+         os_CODENAME="snow leopard"
+         elif [[ "$os_RELEASE" =~ "10.5" ]]; then
+         os_CODENAME="leopard"
+         elif [[ "$os_RELEASE" =~ "10.4" ]]; then
+         os_CODENAME="tiger"
+         elif [[ "$os_RELEASE" =~ "10.3" ]]; then
+         os_CODENAME="panther"
+         else
+         os_CODENAME=""
+         fi
+         elif [[ -x $(which lsb_release 2>/dev/null) ]]; then
+         os_VENDOR=$(lsb_release -i -s)
+         os_RELEASE=$(lsb_release -r -s)
+         os_UPDATE=""
+         if [[ "Debian,Ubuntu" =~ $os_VENDOR ]]; then
+             os_PACKAGE="deb"
+         else
+             os_PACKAGE="rpm"
+         fi
+         os_CODENAME=$(lsb_release -c -s)
+     elif [[ -r /etc/redhat-release ]]; then
+                                                                                                                                                                        os_CODENAME=""
+         for r in "Red Hat" CentOS Fedora; do
+             os_VENDOR=$r
+             if [[ -n "`grep \"$r\" /etc/redhat-release`" ]]; then
+                  ver=`sed -e 's/^.* \(.*\) (\(.*\)).*$/\1\|\2/' /etc/redhat-release`
+                  os_CODENAME=${ver#*|}
+                  os_RELEASE=${ver%|*}
+                  os_UPDATE=${os_RELEASE##*.}
+                  os_RELEASE=${os_RELEASE%.*}
+                  break
+             fi
+            os_VENDOR=""
+        done
+        os_PACKAGE="rpm"
+    fi
+   export os_VENDOR os_RELEASE os_UPDATE os_PACKAGE os_CODENAME
+}
 
-
-
-GetDistro
 
 
 
 function GetDistro() {
-    GetOSVersion
         if [[ "$os_VENDOR" =~ (Ubuntu) ]]; then
               DISTRO=$os_CODENAME
         elif [[ "$os_VENDOR" =~ (Fedora) ]]; then
@@ -30,3 +74,4 @@ if [[ ! ${DISTRO} =~ (oneiric|precise|quantal|f16|f17) ]]; then
                                     fi
                                 fi
 
+GetDistro
