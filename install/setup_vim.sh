b0c8f6ac55f76270e26d39657bc8a8233f74393b
JamesKnott <jknott@boojookoo.com>
2012-08-19 00:52:40 -0500

    Added vim setup script

---
+++ install/setup_vim.sh
@@ -0,0 +1,8 @@
+git clone git://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+
+echo "map <F2> :NERDTreeToggle<CR>" >> ~/.vimrc
+echo "let NERDTreeShowHidden=1" >> ~/.vimrc
+
+
