b42ca21248978f79dc2710a5d1a1d55302ec91e5
jknott <jknott@Mos-busybox-U1104.(none)>
2012-08-21 15:45:25 -0500

    Fixed .vimrc and added nerdtree toggle

--- install/setup_vim.sh
+++ install/setup_vim.sh
@@ -2,7 +2,7 @@ git clone git://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
 
-echo "map <F2> :NERDTreeToggle<CR>" >> ~/.vimrc
-echo "let NERDTreeShowHidden=1" >> ~/.vimrc
+echo "map <F2> :NERDTreeToggle<CR>" >> ~/.vim_runtime/my_configs.vim
+echo "let NERDTreeShowHidden=1" >> ~/.vim_runtime/my_configs.vim
 
 
