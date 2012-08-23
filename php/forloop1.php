c29802755fb64f34a3cccc183955d34cb40c3f01
JamesKnott <jknott@boojookoo.com>
2012-08-23 14:47:45 -0500

    more php fun

---
+++ php/forloop1.php
@@ -0,0 +1,12 @@
+<?
+
+echo "<select name=dropox size=1>";
+for ($i=1;$i<100;$i++){
+    echo "<option value=$i>$i</option>";
+}
+
+echo "</select>";
+
+
+
+?>
---
+++ php/ifthen1.php
@@ -0,0 +1,19 @@
+<?
+
+$var1 = 1;
+$var2 = 2;
+
+
+if ($var1 == $var2) {
+    echo "They Match!", "\n";
+}
+elseif ( $var1 != $var2) {
+    echo "They do not match", "\n";
+}
+
+
+
+
+
+
+?>
