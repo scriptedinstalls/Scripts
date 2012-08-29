3bde16d36a97441533e09ace8bac4926c6eabe42
JamesKnott <jknott@boojookoo.com>
2012-08-29 13:01:21 -0500

    added php file

--- php/app1/contact_us.php
+++ php/app1/contact_us.php
@@ -18,18 +18,38 @@ $title = "Our first dynamically included site....";
         <tr><td>&nbsp;</td></tr>
         <tr><td>&nbsp;</td></tr>
         <tr><td>&nbsp;</td></tr>
-
-
-
-
+        
         <tr>
             <td align = 'left'><? include 'leftnav.php';?></td>
         </tr>
+        <tr><td>&nbsp;</td></tr>
+        <tr><td>&nbsp;</td></tr>
+        <tr><td>&nbsp;</td></tr>
+        <tr><td>&nbsp;</td></tr>
+        <tr><td>&nbsp;</td></tr>
+
+        </table>
         <table width="100%">
-        <tr>
              <td><form action='action1.php' method='post'></td>
-             <td>Name:</td>
+             <tr><td>Name:</td><td><input name='name' type='text'size='25'></td></tr>
+             <tr><td>Age:</td>
+             <td><select name=age size=1>
+             <? for ($i=18;$i<=65;$i++){
+                echo "<option value=$i>$i"; 
+                } ?>
+             </select>
+        </td>
         </tr>
+            <tr><td>Year Born?:</td>
+        <td>
+        <select name=yearborn size=1>
+            <? for ($i=1939;$i<=1989;$i++) {
+            echo "<option value=$i>$i";
+            } ?>
+        </select>
+        </td>
+        </tr>
+        </form></table>
        
         <tr><td>&nbsp;</td></tr>
         <tr><td>&nbsp;</td></tr>
