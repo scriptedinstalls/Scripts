a61fe3f6dead9795932589b73fdf12486e428871
JamesKnott <jknott@boojookoo.com>
2012-08-31 07:27:39 -0500

    New php files

---
+++ php/form3.php
@@ -0,0 +1,21 @@
+<html>
+
+<head><title>Our second php form</title></head>
+
+<body>
+
+<form action=post3.php method=post enctype=multipart/form-data>
+
+First Name: <input type='text' name=fname size="25"><br>
+Last Name: <input type='text' name=lname size="25"><br>
+Age: <input type='text' name=age size="25"><br>
+State: <input type='text' name=location size="2"><br>
+File: <input type=file name=file1><br>
+<input type=submit value=Submit>
+
+</form>
+
+
+</body>
+
+</html>
---
+++ php/post3.php
@@ -0,0 +1,9 @@
+<?
+
+$f1 = "file1";
+
+echo $_FILES[$f1]['name'], "<br>";
+echo $_FILES[$f1]['tmp_name'], "<br>";
+
+
+?>
