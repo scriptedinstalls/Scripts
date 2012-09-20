c6481344df3aef84f0e96a51abb6245c9e7e7586
JamesKnott <jknott@boojookoo.com>
2012-09-19 20:09:27 -0500

    new net pcap files

---
+++ perl/snooping/test1.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl -w
+use strict;
+use Net::PcapUtils;
+$,=' ';
+
+my $error = Net::PcapUtils::loop(\&print_packet,
+                                 NUMPACKETS => 10,
+                                 );
+die $error if $error;
+
+sub print_packet {
+  my($user_data, $header, $packet) = @_;    
+  my $len = length $packet;
+  my $i=0;
+  do {    
+    my $lg = substr $packet, $i, 16;
+    printf "%.8X : ", $i;
+    $i+=16;
+    print unpack ('H2'x16, $lg), '  'x(16-length $lg);
+    $lg =~ s/[\x00-\x1F\xFF]/./g;
+    print " $lg\n";
+  } until $i>=$len;
+  print "\n";
+}
